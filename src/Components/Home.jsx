c3f995082b8ed179b3dcbf8dbaa3570df604ecdb
orrin-temp <zummsg@gmail.com>
2024-03-21 18:48:52 +0530

    update

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from "react";
 import Row from "./Row";
 import axios from "axios";
+import { FaPlay } from "react-icons/fa";
 const apikey = "7f803f6abb22fd06057fed554c09cf75";
 const url = "https://api.themoviedb.org/3/movie/";
 
 function Home() {
-
   // popular movies
   const [popularMovies, setPopularMovies] = useState([]);
   useEffect(() => {
@@ -48,11 +48,24 @@ function Home() {
 
   return (
     <div className="home">
-      <div className="banner"></div>
-      <Row title="Upcoming" arr={upcomingMovies}/>
-      <Row title="Popular" arr={popularMovies}/>
-      <Row title="Now Playing" arr={now_playingMovies}/>
-      <Row title="Top Rated" arr={top_ratedMovies}/>
+      <div className="banner">
+        <div className="heading">
+          <h1>STRANGER THINGS</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque dolore assumenda eaque totam odit perspiciatis autem rerum, quae quo deleniti molestiae iusto corrupti quas modi cupiditate, facere commodi exercitationem delectus.</p>
+          <button className="btn"><FaPlay /><h4>  PLAY</h4></button>
+        </div>
+        <div className="shadow"></div>
+        <video className="bg" width="100%" height="100%" autoPlay loop muted>
+          <source
+            src="https://raw.githubusercontent.com/orrin-temp/background-video/main/New%20Project.mp4"
+            type="video/mp4"
+          />
+        </video>
+      </div>
+      <Row title="Upcoming" arr={upcomingMovies} />
+      <Row title="Popular" arr={popularMovies} />
+      <Row title="Now Playing" arr={now_playingMovies} />
+      <Row title="Top Rated" arr={top_ratedMovies} />
     </div>
   );
 }
